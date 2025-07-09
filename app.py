e7e22a53045daba2115096096cac01dab4243a8d
Alokzhan <alokverma7985354732@gmail.com>
2025-07-09 10:53:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -177,5 +177,5 @@ def download_file(filename):
         return jsonify({'error': 'File not found'}), 404
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
